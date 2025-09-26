effa26693fee209f64a6934c1961729e2e58d8c1
ramiro <ramirodominguez0220@gmail.com>
2025-09-26 20:46:37 -0300

    presentacion terminada

---
+++ presentacion.py
@@ -0,0 +1,8 @@
+nombre = "ramiro"
+edad = "22"
+hobbie = "futbol"
+
+print("Hola me llamo" + nombre)
+print("tengo" + edad + "años")
+print("mi hobbie es jugar al" + hobbie + "los fines de semana")
+
