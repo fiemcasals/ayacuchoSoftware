dab5175154654eb0a534243a2008b4f8a48f4703
Jeremias-Ponce <jeremiasponce141@gmail.com>
2025-09-30 12:38:22 -0300

    primer trabajo con codigo

---
+++ hola.py
@@ -0,0 +1,14 @@
+# Datos personales en Python
+
+nombre = "Jeremías"
+pais = "Argentina"
+ciudad = "Buenos Aires"
+año = 2025
+edad = 22
+
+print("=== Mi Información ===")
+print("Nombre:", nombre)
+print("País:", pais)
+print("Ciudad:", ciudad)
+print("Año:", año)
+print("Edad:", edad)
