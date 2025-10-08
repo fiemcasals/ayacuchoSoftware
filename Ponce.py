ef9cde38427de8ad83bd2717895cdefe826220f3
Valentinponce <valentinred44@gmail.com>
2025-10-08 20:53:28 -0300

    Presentación

---
+++ Ponce.py
@@ -0,0 +1,9 @@
+Nombre = "Valentín"
+Apellido = "Ponce"
+Edad = 21
+Email = "valentinred44@gmail.com"
+
+print(f"Nombre: {Nombre}")
+print(f"Apellido: {Apellido}")
+print(f"Edad: {Edad}")
+print(f"Email: {Email}")
