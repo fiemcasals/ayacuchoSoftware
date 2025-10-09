de500ea5d06a4b80c70136e33cfa74360125b135
seba99quiro <seba99quiro@gmail.com>
2025-10-09 12:07:54 -0300

    Datos actualizados

--- Datos.py
+++ Datos.py
@@ -1,9 +1,13 @@
-nombre = "sebastian quiroga"
-edad = 26 
-rubro = "vendo y arreglo celulares" 
-padres = "si"
+name = "sebastian quiroga"
+edad =  26 
+rubro = "vendiendo y arreglando celulares" 
+Familia = "Padre, madre y dos hermanas"
+Hobby = "Jugar al futbol/padel y estar con mis amigos"
+Estado_civil = "con agustina"
+Diplomatura = "En este momento estudiando una diplomatura en software"
 
-print(nombre)
-print(edad)
-print(rubro)
+
+print("Mi nombre es", name, ", tengo",  edad, "años y trabajo", rubro, "mi familia esta compuesta por", Familia, "me gusta", Hobby,"estoy de novio", Estado_civil, "Me encuentro", Diplomatura)
+
+
 
