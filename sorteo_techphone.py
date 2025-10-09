168c6541016de04d4da7247a44033769cd58ed4f
seba99quiro <seba99quiro@gmail.com>
2025-10-09 16:18:41 -0300

    Agrego el sorte de techphone

---
+++ sorteo_techphone.py
@@ -0,0 +1,9 @@
+lista_nombres = ["melina_denaro", "colavitaagustina", "luu_videla23", "jacinta_g.aniz", "tibilettimartin"] 
+print(f"la lista es:", lista_nombres)
+import random
+nombre_azar = random.choice(lista_nombres)
+print(f"el ganador es", {nombre_azar})
+
+
+
+
